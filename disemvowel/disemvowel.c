736526e7074bfe1ab4820883dc24f4f42fc9a689
walte774 <walte774@morris.umn.edu>
2019-09-19 16:29:47 -0500

    Write disemvowel.c
    This is our first stab at things, and isn't complete.
    
    Co-authored-by: merki002 <merki002@morris.umn.edu>
    Co-authored-by: AaronCorpstein <corps002@morris.umn.edu>

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,40 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  char* novowels;
+  int inputlen;
+  int outputlen;
+  char vowels[10] = {'a','e','i','o','u','A','E','I','O','U'};
+  inputlen = strlen(str);
+  outputlen = inputlen;
+  for (int i=0; i<inputlen; i++) {
+    for (int v=0; v<10; v++) {
+       if (str[i] == vowels[v]) {
+         outputlen--;
+       }
+    }
+  }
+  int novindex = 0;
+  novowels = calloc(outputlen + 1, sizeof(char));
+  for (int i=0; i<inputlen; i++) {
+	  bool isvowel = false;
+	  for (int v=0; v<10; v++) {
+		  if (str[i] == vowels[v]) {
+			  isvowel = true;
+		  } if (isvowel) {
+			  break;
+		  }
+	  }
+	  if (!isvowel) {
+		  novowels[novindex] = str[i];
+		  novindex++;
+	  }
+  }
+  novowels[outputlen+1] = '\0';
+  return novowels;
 }
