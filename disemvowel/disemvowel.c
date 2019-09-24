17d9493fd217dea7ec242cc4bc73b6785ea0759b
walte774 <walte774@morris.umn.edu>
2019-09-24 10:39:07 -0500

    Add clarifying comments to disemvowel.c
    Co-authored-by: merki002 <merki002@morris.umn.edu>
    Co-authored-by: AaronCorpstein <corps002@morris.umn.edu>

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -6,35 +6,35 @@
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  char* novowels;
+  char* novowels; //What we intend to return
   int inputlen;
   int outputlen;
   char vowels[10] = {'a','e','i','o','u','A','E','I','O','U'};
-  inputlen = strlen(str);
+  inputlen = strlen(str); //Length of input, which doesn't include the null terminator
   outputlen = inputlen;
-  for (int i=0; i<inputlen; i++) {
-    for (int v=0; v<10; v++) {
+  for (int i=0; i<inputlen; i++) { //We go through the input string...
+    for (int v=0; v<10; v++) { //Check if that character is a vowel...
        if (str[i] == vowels[v]) {
-         outputlen--;
+         outputlen--; //And shrink the size of the output string appropriately
        }
     }
   }
-  int novindex = 0;
-  novowels = (char*)calloc(outputlen + 1, sizeof(char));
+  int novindex = 0; //This variable tracks what index we're inserting characters into our output string novowels
+  novowels = (char*)calloc(outputlen + 1, sizeof(char)); //Size is outputlen + 1 to account for the null terminator
   for (int i=0; i<inputlen; i++) {
-	  bool isvowel = false;
+	  bool isvowel = false; //We assume the character isn't a vowel until proven otherwise
 	  for (int v=0; v<10; v++) {
-		  if (str[i] == vowels[v]) {
+		  if (str[i] == vowels[v]) { //We check if the character is a particular vowel
 			  isvowel = true;
-		  } if (isvowel) {
+		  } if (isvowel) { //If it is, we exit the loop
 			  break;
 		  }
 	  }
-	  if (!isvowel) {
+	  if (!isvowel) { //If the character is not a vowel, we put it in the appropriate space in our output string, then increment that index
 		  novowels[novindex] = str[i];
 		  novindex++;
 	  }
   }
-  novowels[outputlen] = '\0';
+  novowels[outputlen] = '\0'; //We add the null terminator
   return novowels;
 }
